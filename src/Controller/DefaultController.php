57ba4e2c09e8cafefd0d9dedd001b58e9014f1e3
rernesto <rernesto.espinosa@gmail.com>
2024-02-25 20:55:16 -0300

    feat (app): initial files
    Demo microservice files
    
    Refs: #1

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Demo\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    #[Route('/', name: 'demo_default_index')]
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new demo controller!',
+            'path' => 'apps/demo/src/Controller/DefaultController.php',
+        ]);
+    }
+}
