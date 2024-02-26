0abef61453be287c6b5bc98c1f9a7aefc035e967
rernesto <rernesto.espinosa@gmail.com>
2024-02-25 21:06:53 -0300

    feat (app): namespace deprecation
    Replaced namespace use `Symfony\Component\Routing\Annotation\Route` with `Symfony\Component\Routing\Attribute\Route`
    
    Refs: #1

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,7 +4,7 @@ namespace Demo\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 class DefaultController extends AbstractController
 {
